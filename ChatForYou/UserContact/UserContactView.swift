1e6faa4ec2b5228e7bb1386ed1176bcd6579771d
Oleksandr <elmart847483@gmail.com>
2022-06-07 22:42:45 +0300

    Create user contact view

--- ChatForYou/UserContact/UserContactView.swift
+++ ChatForYou/UserContact/UserContactView.swift
@@ -6,15 +6,95 @@
 //
 
 import SwiftUI
+struct NavigationConfigurator: UIViewControllerRepresentable {
+    var configure: (UINavigationController) -> Void = { _ in }
 
+    func makeUIViewController(context: UIViewControllerRepresentableContext<NavigationConfigurator>) -> UIViewController {
+        UIViewController()
+    }
+    func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<NavigationConfigurator>) {
+        if let nc = uiViewController.navigationController {
+            self.configure(nc)
+        }
+    }
+
+}
 struct UserContactView: View {
+    init() {
+        UITableView.appearance().backgroundColor = .clear // For tableView
+        UITableViewCell.appearance().backgroundColor = .clear // For tableViewCell
+       
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List(contacts) { contact in
+                ContactRow(contact: contact)
+                    .listRowBackground(Color.clear)
+            }
+            .background(Image("backgRoundChat"))
+            .navigationBarTitle("Yor contact", displayMode: .large)
+                      .background(NavigationConfigurator { nc in
+                          nc.navigationBar.barTintColor = .blue
+                          nc.navigationBar.titleTextAttributes = [.foregroundColor : UIColor.white]
+                      })
+            .toolbar {
+                ToolbarItemGroup(placement: .bottomBar) {
+                    Button(action: {}) {
+                        Image("user")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 60, height: 60)
+                            .clipped()
+                            .cornerRadius(50)
+                            .padding([.top],20)
+                        
+                    }
+                    
+                    Button(action: {}) {
+                        Image("chat")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 60, height: 60)
+                            .clipped()
+                            .cornerRadius(50)
+                            .padding([.top],20)
+                        
+                    }
+                }
+                
+            }
+            
+        }
+        
     }
 }
 
 struct UserContactView_Previews: PreviewProvider {
     static var previews: some View {
         UserContactView()
+    }
+}
+
+struct ContactRow: View {
+    let contact: Contact
+    
+    var body: some View {
+        HStack {
+            Image(contact.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 60, height: 60)
+                .clipped()
+                .cornerRadius(50)
+            
+            Spacer()
+            VStack(alignment: .trailing) {
+                Text(contact.name)
+                    .font(.system(size: 21, weight: .medium, design: .default))
+                Text(contact.phone)
+                
+            }
+            
+        }
     }
 }
---
+++ ChatForYou/UserContact/UserContactViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  UserContactViewModel.swift
+//  ChatForYou
+//
+//  Created by admin on 07.06.2022.
+//
+
+import Foundation
+
+struct Contact: Identifiable {
+    let id = UUID()
+    let imageName: String
+    let name: String
+    let phone: String
+    let email: String
+    let address: String
+}
+
+let contacts = [
+    Contact(imageName: "1", name: "Holly F. Huey", phone: "+1(242)-8110134", email: "hollyhuey@yeilmail.com", address: "242 Wildrose River 16040 Louisiana"),
+    Contact(imageName: "1", name: "Rose Acker", phone: "+1(656)-1881047", email: "roseacker@neymail.com", address: "249 Modoc Half 75290 Michigan"),
+    Contact(imageName: "1", name: "Leonardo Longnecker", phone: "+1(545)-3442899", email: "cbleonardo1@zuimail.com", address: "952 Baker Haggerty 90562 Missouri"),
+    Contact(imageName: "1", name: "Quentin F. Joplin", phone: "+1(434)-7448466", email: "cfjoplin5@neymail.com", address: "176 Flanigan Road 49223 Mississippi"),
+    Contact(imageName: "1", name: "Christine Clapper", phone: "+1(141)-5115553", email: "dachristine0@vuomail.com", address: "635 Prospect River 58641 Kansas"),
+    Contact(imageName: "1", name: "Holly F. Huey", phone: "+1(242)-8110134", email: "hollyhuey@yeilmail.com", address: "242 Wildrose River 16040 Louisiana"),
+    Contact(imageName: "1", name: "Rose Acker", phone: "+1(656)-1881047", email: "roseacker@neymail.com", address: "249 Modoc Half 75290 Michigan"),
+    Contact(imageName: "1", name: "Leonardo Longnecker", phone: "+1(545)-3442899", email: "cbleonardo1@zuimail.com", address: "952 Baker Haggerty 90562 Missouri"),
+    Contact(imageName: "1", name: "Quentin F. Joplin", phone: "+1(434)-7448466", email: "cfjoplin5@neymail.com", address: "176 Flanigan Road 49223 Mississippi"),
+    Contact(imageName: "1", name: "Christine Clapper", phone: "+1(141)-5115553", email: "dachristine0@vuomail.com", address: "635 Prospect River 58641 Kansas"),
+    Contact(imageName: "1", name: "Holly F. Huey", phone: "+1(242)-8110134", email: "hollyhuey@yeilmail.com", address: "242 Wildrose River 16040 Louisiana"),
+    Contact(imageName: "1", name: "Rose Acker", phone: "+1(656)-1881047", email: "roseacker@neymail.com", address: "249 Modoc Half 75290 Michigan"),
+    Contact(imageName: "1", name: "Leonardo Longnecker", phone: "+1(545)-3442899", email: "cbleonardo1@zuimail.com", address: "952 Baker Haggerty 90562 Missouri"),
+    Contact(imageName: "1", name: "Quentin F. Joplin", phone: "+1(434)-7448466", email: "cfjoplin5@neymail.com", address: "176 Flanigan Road 49223 Mississippi"),
+    Contact(imageName: "1", name: "Christine Clapper", phone: "+1(141)-5115553", email: "dachristine0@vuomail.com", address: "635 Prospect River 58641 Kansas"),
+    Contact(imageName: "1", name: "Joy Cordon", phone: "+1(353)-0663954", email: "bvjoy21@ypmail.com", address: "763 University Trail 81701 Wisconsin")
+]
